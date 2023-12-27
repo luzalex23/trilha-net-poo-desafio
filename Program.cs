c052c0401f73a5fd1fc3eb5cdb8c12b89ebe29fe
luzalex23 <alexluzendreyravi@gmail.com>
2023-12-27 18:25:16 -0300

    Desáfio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Bem vindo ao Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "7199875", modelo: "M-75", imei:"22321", memoria: 128);
+nokia.Ligar();
+ Console.BackgroundColor = ConsoleColor.Green;
+nokia.InstalarAplicativo("whatsapp");
+Console.WriteLine("\n");
+
+
+Console.WriteLine("Bem vindo ao Seu novo Iphone:");
+Smartphone iphone = new Iphone(numero:"001235", modelo: "S20", imei:"5545", memoria:256);
+iphone.ReceberLigacao();
+ Console.BackgroundColor = ConsoleColor.DarkMagenta;
+iphone.InstalarAplicativo("Instagran");
+Console.WriteLine("\n");
